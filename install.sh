04be967e0bd3a6a7ae1a1933f91ab29174a57e70
Bryce Kellogg <brycekellogg@brycekellogg.com>
2019-03-26 13:30:34 -0700

    Added script for setting up

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/sh
+
+# NeoVim
+pip3 install --user neovim
+
+# Universal CTags
+git clone https://github.com/universal-ctags/ctags.git
+cd ctags
+./autogen.sh
+./configure
+make
+sudo make install
+rm -rf ctags
+
+# Dotfiles
+echo """
+ZDOTDIR=$HOME/.dotfiles/zsh
+source $ZDOTDIR/.zshenv
+""" > $HOME/.zshenv
+
+# Git Config
+git config --global include.path ~/.dotfiles/.gitconfig
+
+# SSH Config
+ln -s ~/.dotfiles/ssh-config ~/.ssh/config
+
+# Dircolors
+ln -s ~/.dotfiles/.dir_colors ~/.dir_colors
