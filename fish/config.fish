819ffcaffaf2cdf49ae14c5077fc3c337e1a8bca
Bryce Kellogg <bryce@kellogg.org>
2021-07-19 10:01:35 -0700

    Updated fish config

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,7 @@ set -x PATH ~/.local/bin/ ~/bin/ $PATH
 alias vim='nvim'
 alias cb='xclip -selection clipboard'
 alias ll='ls -l -G -X -h --group-directories-first'
-alias man='man -P "nvim --startuptime man.log -u ~/.dotfiles/pager.vim -R -c \'set ft=man\' -"'
+
 
 # Ignoring Key Combinations
 bind \e\[6\;5~ ''  # CTRL-PAGEDOWN
@@ -27,32 +27,31 @@ bind \e\[2~ ''     # INSERT
 # Neovim Configuration
 set -x VIMINIT source "~/.dotfiles/init.vim"
 set -x EDITOR nvim
-set -x PAGER "nvim -u ~/.dotfiles/pager.vim -R -"
 
-# Tmux Titles
+
+# Set terminal (Tmux) title
+#
+# Here we overwrite a fish shell built in function that sets the
+# title of the terminal emulator, in this case Tmux. If we're
+# anywhere inside a git repo, we want the title to be set to the
+# name of the git repo. Otherwise we want the title to be set to
+# the name of the current working directory.
 function fish_title
-    if git rev-parse --git-dir > /dev/null ^ /dev/null
-        set -l gitdir (git rev-parse --git-dir)
-        if test $gitdir = ".git"
-            echo (basename (pwd))
-        else
-            echo (basename (dirname $gitdir))
-        end
-    else
+    set -l gitdir (git rev-parse --git-dir 2> /dev/null)
+    if test "$gitdir" = ""
         echo (basename (pwd))
+    else if test "$gitdir" = ".git"
+        echo (basename (pwd))
+    else
+        echo (basename (dirname "$gitdir"))
     end
 end
 
 
 function fish_prompt --description 'Write out the prompt'
-
-
-    # Only display USER@HOSTNAME if connected over ssh
-    #if set -g SSH_TTY
-        #echo -n -s "$USER"@(prompt_hostname)" "
-    #end
-
     echo -n -s (set_color $fish_color_cwd) (prompt_pwd) \
                (set_color $fish_color_vcs) (__fish_vcs_prompt) \
                (set_color $fish_color_pmt) " » "
 end
+
+direnv hook fish | source
